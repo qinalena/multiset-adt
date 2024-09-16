5c1470ed2134dbbfd4093956c9e7191008675f9d
Junyu-li-777 <152134523+Junyu-li-777@users.noreply.github.com>
2024-09-16 12:42:33 -0400

    some code for TreeMultiSet

--- src/TreeMultiSet.java
+++ src/TreeMultiSet.java
@@ -1,4 +1,5 @@
 public class TreeMultiSet extends MultiSet {
+    private Tree tree
 
     // TODO add a Tree object as a private instance variable for this class.
     /**
@@ -8,8 +9,17 @@ public class TreeMultiSet extends MultiSet {
      */
     @Override
     void add(Integer item) {
+        tree.add(item);
         // TODO complete this method
     }
     // TODO write the rest of the implementation for this class so that it uses its private Tree
     //      object to provide the MultiSet functionality.
+
+    void remove(Integer item) {
+        tree.delete_item(item);
+    }
+
+    boolean contains(Integer item) {
+        return tree.contains(item);
+    }
 }
